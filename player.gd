0c4205574c1a1fb7f529c3538ff3e75f3c91e136
AJaxx <62261949+AJaxx86@users.noreply.github.com>
2025-11-27 17:28:49 +0000

    fixed moving while paused

--- player.gd
+++ player.gd
@@ -33,6 +33,8 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	if is_paused:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 		move_and_slide()
 		return
 
